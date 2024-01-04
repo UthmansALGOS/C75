56a569b547d96e52cdcc4c9761afbfe0a2dbb179
UthmansALGOS <96424036+UthmansALGOS@users.noreply.github.com>
2024-01-04 19:11:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import BottomTabNavigtor from "./components/BottomTabNavigator";
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+import "react-native-gesture-handler";
+
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      fontLoaded : false
+    }
+  }
+
+  async loadFonts(){
+    await Font.loadAsync({
+      Rajdhani_600SemiBold : Rajdhani_600SemiBold 
+    })
+
+      this.setState({fontLoaded : true})
+  }
+
+  componentDidMount(){
+    this.loadFonts();
+  }
+
+  
+  render(){
+  const {fontLoaded} = this.state;
+  if(fontLoaded){
+    return (
+      <BottomTabNavigtor/>
+    );
+  }
+  return null;
+  }
+}
+
+
